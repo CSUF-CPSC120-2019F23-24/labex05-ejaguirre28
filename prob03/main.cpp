3f2b292b400edcccf8e5c5da123183e559ade42a
ejaguirre <ejaguirre@cs104u-3c.acad.fullerton.edu>
2019-09-21 14:08:07 -0700

    Created a program that randomizes a rectangle at different lengths.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,26 @@
 // Random Rectangle
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <cstdlib>
+
+int main()
+{
+  //sets the limit of which the string can be randomized
+  int max= 40;
+  int num;
+  unsigned seed = time(0);
+  srand(seed);
+  num = rand() % max + 1;
+
+  //assigning # to be randomized
+  std::string rec;
+  rec.assign(num, '#');
+
+  //displays the Rectangle
+  std::cout << rec << std::endl;
+  std::cout << rec << std::endl;
+  std::cout << rec << std::endl;
+
+  return 0;
+}
